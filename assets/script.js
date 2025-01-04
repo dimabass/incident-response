129c9ae7b309e9c34f90d923b743c147be5c66b7
dimabass <122013518+dimabass@users.noreply.github.com>
2025-01-04 23:03:07 +0100

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,10 @@
+document.querySelectorAll('img').forEach(function(image) {
+  image.classList.add('zoomable');
+  
+  image.addEventListener('click', function() {
+    const imageUrl = image.src;
+    const imageWindow = window.open();
+    imageWindow.document.write(`<img src="${imageUrl}" style="width: 100%; height: auto;"/>`);
+  });
+});
 
